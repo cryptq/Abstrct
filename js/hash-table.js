f36cc4a6ab4000a5cae8d6056a5061ea90a0b3df
Egor Rumyantsev <RumyantsevGT@ya.ru>
2018-02-04 21:31:16 +0300

    Update hash-table.js

--- js/hash-table.js
+++ js/hash-table.js
@@ -23,3 +23,62 @@
  *
  * @module data-structures/hash-table
 */
+(function (exports) {
+  'use strict';
+
+  /**
+   * Constructs a Node to store data and next/prev nodes in Hash table.
+   *
+   * @public
+   * @constructor
+   * @param {Number|String} key Key of the node.
+   * @param {Number|String} data Data to be stored in hash table.
+   */
+  exports.Node = function (key, data) {
+    this.key = key;
+    this.data = data;
+    this.next = undefined;
+    this.prev = undefined;
+  };
+
+  /**
+   * Construct a Hash table..
+   *
+   * @public
+   * @constructor
+   */
+  exports.Hashtable = function () {
+    this.buckets = [];
+    // The higher the bucket count; less likely for collisions.
+    this.maxBucketCount = 100;
+  };
+
+  /**
+   * Simple non-crypto hash used to hash keys, which determines
+   * while bucket the value will be placed in.
+   * A javascript implementation of Java's 32bitint hash.
+   *
+   * @public
+   * @method
+   * @param {Number|String} val Key to be hashed.
+   */
+  exports.Hashtable.prototype.hashCode = function (val) {
+    var i;
+    var hashCode = 0;
+    var character;
+
+    // If value to be hashed is already an integer, return it.
+    if (val.length === 0 || val.length === undefined) {
+      return val;
+    }
+
+    for (i = 0; i < val.length; i += 1) {
+      character = val.charCodeAt(i);
+      /*jshint -W016 */
+      hashCode = ((hashCode << 5) - hashCode) + character;
+      hashCode = hashCode & hashCode;
+      /*jshint -W016 */
+    }
+
+    return hashCode;
+  };
