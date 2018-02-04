bdc2cff1af1925b20d369fa1b1d502bcd7c879ff
Egor Rumyantsev <RumyantsevGT@ya.ru>
2018-02-04 19:33:43 +0300

    Create hash-table.js

---
+++ js/hash-table.js
@@ -0,0 +1,25 @@
+/**
+ * Hash Table
+ *
+ * An associative array, that can map keys
+ * (strings and numbers) to values in O(1).
+ *
+ * @example
+ * var hash = require('path-to-algorithms/src/data-structures'+
+ * '/hash-table');
+ * var hashTable = new hash.Hashtable();
+ *
+ * hashTable.put(10, 'value');
+ * hashTable.put('key', 10);
+ *
+ * console.log(hashTable.get(10)); // 'value'
+ * console.log(hashTable.get('key')); // 10
+ *
+ * hashTable.remove(10);
+ * hashTable.remove('key');
+ *
+ * console.log(hashTable.get(10)); // undefined
+ * console.log(hashTable.get('key')); // undefined
+ *
+ * @module data-structures/hash-table
+*/
