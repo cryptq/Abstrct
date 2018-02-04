8938d7528433349b63c034127157b66456c8b6b8
Egor Rumyantsev <RumyantsevGT@ya.ru>
2018-02-05 02:19:12 +0300

    Update timestamp.js

--- js/snippets/timestamp.js
+++ js/snippets/timestamp.js
@@ -52,3 +52,55 @@ function date_time() {
     })())
   );
 }
+
+
+function date_time() {
+  Data_Now = new Date();
+  Year = Data_Now.getFullYear();
+  Month = Data_Now.getMonth();
+  Day = Data_Now.getDate();
+  Hour = Data_Now.getHours();
+  Minutes = Data_Now.getMinutes();
+  Seconds = Data_Now.getSeconds();
+  switch (Month) {
+    case 0:
+      fMonth = "января";
+      break;
+    case 1:
+      fMonth = "февраля";
+      break;
+    case 2:
+      fMonth = "марта";
+      break;
+    case 3:
+      fMonth = "апреля";
+      break;
+    case 4:
+      fMonth = "мая";
+      break;
+    case 5:
+      fMonth = "июня";
+      break;
+    case 6:
+      fMonth = "июля";
+      break;
+    case 7:
+      fMonth = "августа";
+      break;
+    case 8:
+      fMonth = "сентября";
+      break;
+    case 9:
+      fMonth = "октября";
+      break;
+    case 10:
+      fMonth = "ноября";
+      break;
+    case 11:
+      fMonth = "декабря";
+      break;  }
+  return dt();
+  function dt() {
+    return Day + " " + fMonth + " " + Year + ' года'+' '+ Hour + ":" + Minutes + ":" + Seconds;
+  }
+}
