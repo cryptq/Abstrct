d961b2aa818a6f1781fe54228981c316a6b2e455
Egor Rumyantsev <RumyantsevGT@ya.ru>
2018-02-05 06:23:07 +0300

    Create block.js

---
+++ js/snippets/block.js
@@ -0,0 +1,27 @@
+class Block
+{
+	constructor(index, timeStamp, data, previousHash)
+	{
+		this.index = index;                                                                                                             
+		this.timeStamp = timeStamp;                                                                                                                 
+		this.data = data;
+		this.previousHash = previousHash;
+		this.hash = this.calculateHash();
+    		this.nonce = 0;                         //Случайное значение, используемое при расчете добычи
+	}
+
+	calculateHash()						                    //Используем sha256 алгоритм для создания хэша на основе переданной строки
+	{
+  		return CryptoJS.SHA256(this.index + this.previousHash + this.timeStamp + JSON.stringify(this.data) + this.nonce).toString();
+	}
+  
+  mineBlock(difficulty)                         //Доказываем выполнение работы "Пруф оф ворк"
+  {
+    while(this.hash.substring(0,difficulty) !== Array(difficulty + 1).join("0"))
+      {
+        this.nonce++;
+        this.hash = this.calculateHash();
+      }
+    console.log("блок успешно сгенерирован с хэшем : " + this.hash);
+  }
+}
