efa75942a6efd9b7fd08cccdb89fa3e2fac64b1a
Egor Rumyantsev <RumyantsevGT@ya.ru>
2018-02-06 04:33:09 +0300

    Update _headers.php

--- code/_headers.php
+++ code/_headers.php
@@ -16,4 +16,3 @@ $headers[] = "Connection: keep-alive";
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 $result = curl_exec($ch);
 $abc = $result[9].$result[10].$result[11].$result[12].$result[13].$result[14].$result[15].$result[16].$result[17].$result[18].$result[19].$result[20].$result[21].$result[22].$result[23].$result[24].$result[25].$result[26].$result[27].$result[28].$result[29].$result[30].$result[31].$result[32].$result[33].$result[34].$result[35].$result[36].$result[37].$result[38].$result[39].$result[40].$result[41].$result[42].$result[43].$result[44].$result[45].$result[46].$result[47].$result[48].$result[49].$result[50].$result[51].$result[52].$result[53].$result[54];
-?>
