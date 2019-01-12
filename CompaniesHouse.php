3cc8783f9c2381c7efcd0b62d3349139385b0ff9
Egor Rumyantsev <RumyantsevGT@ya.ru>
2019-01-12 03:06:03 +0300

    Update CompaniesHouse.php

--- CompaniesHouse.php
+++ CompaniesHouse.php
@@ -3,9 +3,9 @@
 	/**
 	*
         * @param (string)         private $_ApiKey = 'YOUR_COMPANIES_HOUSE_API_KEY';
-        * @param                  require_once('CompaniesHouse.php');
-        * @param                  $CompaniesHouse = new CompaniesHouse; var_dump($CompaniesHouse->company_profile('10131005'));
-	*
+        *                   require_once('CompaniesHouse.php');
+        *           $CompaniesHouse = new CompaniesHouse; var_dump($CompaniesHouse->company_profile('OC391410'));
+	*                         
 	*/
 
 	define('VERSION', '1.00');
